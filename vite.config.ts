fc2fe9cbd00ad97165e42f1e13dc3289bfb1c17e
Deependra jhariya <107026547+Deependra-jhariya@users.noreply.github.com>
2025-05-12 23:40:08 +0530

    deploy the project
    done

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,20 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
-  ],
+  // plugins: [
+  //   react(),
+  //   runtimeErrorOverlay(),
+  //   ...(process.env.NODE_ENV !== "production" &&
+  //   process.env.REPL_ID !== undefined
+  //     ? [
+  //         await import("@replit/vite-plugin-cartographer").then((m) =>
+  //           m.cartographer(),
+  //         ),
+  //       ]
+  //     : []),
+  // ],
+  plugins: [react()],
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
